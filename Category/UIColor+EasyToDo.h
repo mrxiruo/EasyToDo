00fd973ae664663c5721fe780ce8d5df9390f80a
夕若 <mr_xi@foxmail.com>
2015-10-13 23:44:48 +0800

    添加UIColor类别

---
+++ Category/UIColor+EasyToDo.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+EasyToDo.h
+//  EasyToDo
+//
+//  Created by XiRuo on 15/10/13.
+//  Copyright © 2015年 Xiruo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (EasyToDo)
+
++ (UIColor *)colorWithRGBHex:(UInt32)hex;
+
+@end
