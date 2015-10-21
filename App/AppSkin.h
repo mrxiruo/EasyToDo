f06b032dec49a5e606be302f8bdd68a84fafc8da
夕若 <mr_xi@foxmail.com>
2015-10-21 21:22:47 +0800

    Add AppSkin

---
+++ App/AppSkin.h
@@ -0,0 +1,16 @@
+//
+//  AppSkin.h
+//  EasyToDo
+//
+//  Created by XiRuo on 15/10/21.
+//  Copyright © 2015年 Xiruo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppSkin : NSObject
+
++ (UIColor *)baseTextColor;
++ (UIColor *)baseBackgroundColor;
+
+@end
---
+++ App/AppSkin.m
@@ -0,0 +1,23 @@
+//
+//  AppSkin.m
+//  EasyToDo
+//
+//  Created by XiRuo on 15/10/21.
+//  Copyright © 2015年 Xiruo. All rights reserved.
+//
+
+#import "AppSkin.h"
+
+@implementation AppSkin
+
++ (UIColor *)baseTextColor
+{
+    return [UIColor colorWithRGBHex:0XF0ECD4];
+}
+
++ (UIColor *)baseBackgroundColor
+{
+    return [UIColor colorWithRGBHex:0X09A28A];
+}
+
+@end
